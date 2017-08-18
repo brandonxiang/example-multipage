caf09b0e849b877b8c838be9aebbf4c73abad7e3
项伟平 <xiangweiping103@pingan.com.cn>
2017-08-18 08:28:48 +0800

    multipage 路径统一

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -7,43 +7,41 @@ var baseWebpackConfig = require('./webpack.base.conf')
 var MultipageWebpackPlugin = require('multipage-webpack-plugin');
 var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 
-
 function resolve(dir) {
   return path.join(__dirname, '..', dir);
 }
 
 // add hot-reload related code to entry chunks
 Object.keys(baseWebpackConfig.entry).forEach(function (name) {
-  baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
-})
+  baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name]);
+});
 
-var webpackConfig = merge(baseWebpackConfig, {
+module.exports = merge(baseWebpackConfig, {
   module: {
-    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
+      rules: utils.styleLoaders({sourceMap: config.dev.cssSourceMap})
   },
+
   // cheap-module-eval-source-map is faster for development
   devtool: '#cheap-module-eval-source-map',
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': config.dev.env
-    }),
-    // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
-    // https://github.com/ampedandwired/html-webpack-plugin
+      new webpack.DefinePlugin({
+          'process.env': config.dev.env
+      }),
 
-    new MultipageWebpackPlugin({
-      bootstrapFilename: 'manifest',
-      templateFilename: 'index.html',
-      templatePath: '[name]',
-      htmlTemplatePath: resolve('src/module/[name]/index.html'),
-      htmlWebpackPluginOptions: {
-          inject: true,
-      }
-    }),
+      // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin(),
 
-    new FriendlyErrorsPlugin()
+      new MultipageWebpackPlugin({
+          bootstrapFilename: 'manifest',
+          templateFilename: '[name].html',
+          templatePath: 'module',
+          htmlTemplatePath: resolve('src/module/[name]/index.html'),
+          htmlWebpackPluginOptions: {
+              inject: true,
+          }
+      }),
+
+      new FriendlyErrorsPlugin()
   ]
-})
-
-module.exports = webpackConfig
+});
